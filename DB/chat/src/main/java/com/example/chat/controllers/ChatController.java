39af0b018646558e0c2c04169fed515476ffc7b1
eleonardo@skillstorm.com <eleonardo@skillstorm.com>
2022-11-07 16:53:57 -0500

    Added cors allow origin

--- DB/chat/src/main/java/com/example/chat/controllers/ChatController.java
+++ DB/chat/src/main/java/com/example/chat/controllers/ChatController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.chat.models.GroupChat;
@@ -16,6 +17,7 @@ import com.example.chat.services.MessageService;
 import com.example.chat.services.UserService;
 
 @RestController
+@CrossOrigin(value = "*")
 public class ChatController {
 
     @Autowired
